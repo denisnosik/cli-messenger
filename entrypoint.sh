95c7ad79c4f9015d01fcbf8bb52c68cde2afb4ec
DenisNosik <denisnosik96@gmail.com>
2026-05-10 15:52:59 +0100

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Waiting for database..."
-until ./goose -dir ./sql/schema -dsn "$DB_URL" postgres up; do
+until ./goose -dir ./sql/schema postgres "$DB_URL" up; do
     echo "Database not ready, retrying in 2s..."
     sleep 2
 done
